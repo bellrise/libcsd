eec1d715b5e1d84c38fccd8ada1e019213125490
bellrise <bellrise.dev@gmail.com>
2022-12-23 20:58:45 +0100

    print: add print() & println() generic routines

---
+++ incl/mint/print.h
@@ -0,0 +1,42 @@
+/* <mint/print.h>
+   Copyright (c) 2022 bellrise */
+
+#pragma once
+#include <mint/str.h>
+
+/* Print functions. */
+
+void print(const void *val);
+void print(const char *val);
+void print(const str &val);
+void print(const char val);
+void print(const unsigned char val);
+void print(const short val);
+void print(const unsigned short val);
+void print(const int val);
+void print(const unsigned int val);
+void print(const long val);
+void print(const unsigned long val);
+void print(const float val);
+void print(const double val);
+
+template <StringConvertible T>
+void print(const T &value)
+{
+	print(value.to_str());
+}
+
+/* Multiple-argument print() functions. */
+
+template <typename... T>
+void print(const T&... values)
+{
+	((print(values), print(' ')), ...);
+}
+
+template <typename... T>
+void println(const T&... values)
+{
+	((print(values), print(' ')), ...);
+	print('\n');
+}
---
+++ src/print.cc
@@ -0,0 +1,71 @@
+/* libmint/src/print.cc
+   Copyright (c) 2022 bellrise */
+
+#include <mint/print.h>
+#include <stdio.h>
+
+
+void print(const void *val)
+{
+	printf("%p", val);
+}
+
+void print(const char *val)
+{
+	printf("%s", val);
+}
+
+void print(const str &val)
+{
+	val.print_to_stdout();
+}
+
+void print(const char val)
+{
+	printf("%c", val);
+}
+
+void print(const unsigned char val)
+{
+	printf("%uc", val);
+}
+
+void print(const short val)
+{
+	printf("%d", val);
+}
+
+void print(const unsigned short val)
+{
+	printf("%ud", val);
+}
+
+void print(const int val)
+{
+	printf("%d", val);
+}
+
+void print(const unsigned int val)
+{
+	printf("%du", val);
+}
+
+void print(const long val)
+{
+	printf("%ld", val);
+}
+
+void print(const unsigned long val)
+{
+	printf("%lu", val);
+}
+
+void print(const float val)
+{
+	printf("%f", val);
+}
+
+void print(const double val)
+{
+	printf("%lf", val);
+}
