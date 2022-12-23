73352b2895818aaf1563e8d289084d999ee24375
bellrise <bellrise.dev@gmail.com>
2022-12-23 20:58:02 +0100

    str: add heap-allocated string

---
+++ incl/mint/str.h
@@ -0,0 +1,67 @@
+/* <mint/str.h>
+   Copyright (c) 2022 bellrise */
+
+#pragma once
+#include <stddef.h>
+#include <stdio.h>
+
+struct str;
+
+/**
+ * @concept StringConvertible
+ * Any type that can be converted to a `str`. Any type returned by T::to_str()
+ * passable to the str::str() constructor is also convertable to a string.
+ * This means that your to_str() method does not need to return a str, but can
+ * instead return a const char*.
+ */
+template <typename T>
+concept StringConvertible = requires(T t, str v)
+{
+	static_cast<str>(t.to_str());
+};
+
+/**
+ * @class str
+ * Heap-allocated string.
+ */
+struct str
+{
+	str();
+	str(str &&moved);
+	str(const str &other);
+	str(const char *string);
+	str(const char *string, size_t maxlen);
+
+	template <StringConvertible T>
+	str(const T &object)
+		: str(object.to_str())
+	{ }
+
+	virtual ~str();
+
+	size_t len() const;
+	virtual str to_str() const;
+	void print_to_stdout() const;
+	str &append(const str &next);
+	str &append(const char *next);
+
+	str &operator=(const str &other);
+	str &operator=(const char *other);
+	str &operator+(const str &next);
+	str &operator+(const char *next);
+	str &operator+=(const str &next);
+	str &operator+=(const char *next);
+
+protected:
+	char *m_ptr;
+	size_t m_space;
+
+	void copy_from(const str &other);
+	void copy_from_raw(const char *other, size_t other_len);
+
+	/* Resize the buffer the string is stored in. This must return the number
+	   of available bytes to use, and not the resized space. This way any caller
+	   functions may check if the next operation will fit in the buffer. resize()
+	   may modify m_space. */
+	[[nodiscard]] virtual size_t resize(size_t nbytes);
+};
---
+++ src/str.cc
@@ -0,0 +1,188 @@
+/* libmint/src/str.cc
+   Copyright (c) 2022 bellrise */
+
+#include <mint/str.h>
+#include <string.h>
+#include <stdio.h>
+#include <new>
+
+str::str()
+	: m_ptr(nullptr)
+	, m_space(0)
+{
+}
+
+str::str(const str &other)
+	: m_ptr(nullptr)
+	, m_space(0)
+{
+	copy_from(other);
+}
+
+str::str(str &&moved)
+	: m_ptr(moved.m_ptr)
+	, m_space(moved.m_space)
+{
+	moved.m_ptr = nullptr;
+	moved.m_space = 0;
+}
+
+str::str(const char *string)
+	: m_ptr(nullptr)
+	, m_space(0)
+{
+	if (string == nullptr)
+		return;
+
+	m_space = strlen(string) + 1;
+	m_ptr = new char[m_space];
+	memcpy(m_ptr, string, m_space);
+	m_ptr[m_space-1] = 0;
+}
+
+str::str(const char *string, size_t maxlen)
+	: m_ptr(nullptr)
+	, m_space(0)
+{
+	copy_from_raw(string, maxlen);
+}
+
+str::~str()
+{
+	delete [] m_ptr;
+}
+
+size_t str::len() const
+{
+	if (m_space == 0)
+		return 0;
+	return m_space - 1;
+}
+
+str str::to_str() const
+{
+	return str(*this);
+}
+
+void str::print_to_stdout() const
+{
+	if (len() == 0)
+		return;
+	printf("%.*s", (int) (m_space - 1), m_ptr);
+}
+
+str &str::append(const str &next)
+{
+	size_t old_len = len();
+	size_t bytes_to_copy = next.len();
+	size_t required_bytes = len() + next.len() + 1;
+
+	if (resize(required_bytes) < required_bytes)
+		bytes_to_copy = next.len() - (m_space - len());
+
+	memmove(&m_ptr[old_len], next.m_ptr, bytes_to_copy);
+	m_ptr[m_space-1] = 0;
+
+	return *this;
+}
+
+str &str::append(const char *next)
+{
+	size_t old_len = len();
+	size_t next_len = strlen(next);
+	size_t bytes_to_copy = next_len;
+	size_t required_bytes = len() + next_len + 1;
+
+	if (resize(required_bytes) < required_bytes)
+		bytes_to_copy = next_len - (m_space - len());
+
+	memmove(&m_ptr[old_len], next, bytes_to_copy);
+	m_ptr[m_space-1] = 0;
+
+	return *this;
+}
+
+void str::copy_from(const str &other)
+{
+	size_t required_bytes = other.len() + 1;
+
+	if (resize(required_bytes) < required_bytes)
+		memmove(m_ptr, other.m_ptr, m_space - 1);
+	else
+		memmove(m_ptr, other.m_ptr, other.len());
+
+	m_ptr[m_space-1] = 0;
+}
+
+void str::copy_from_raw(const char *other, size_t other_len)
+{
+	size_t required_bytes = other_len + 1;
+
+	if (resize(required_bytes) < required_bytes)
+		memmove(m_ptr, other, m_space - 1);
+	else
+		memmove(m_ptr, other, other_len);
+
+	m_ptr[m_space-1] = 0;
+}
+
+size_t str::resize(size_t nbytes)
+{
+	/* Our implementation of resize() only resizes up, without any downsizes.
+	   This means that if we request a small size of bytes, we keep the old
+	   buffer, and alloc a new one only if we don't have enough space. */
+
+	if (m_space >= nbytes)
+		return m_space;
+
+	char *old_ptr = m_ptr;
+
+	try {
+		m_ptr = new char[nbytes+1];
+	} catch (std::bad_alloc &v) {
+		return m_space;
+	}
+
+	/* Copy the memory from the old buffer to the new one. */
+	memmove(m_ptr, old_ptr, m_space);
+	m_space = nbytes;
+
+	delete [] old_ptr;
+	return m_space;
+}
+
+str &str::operator=(const str &other)
+{
+	copy_from(other);
+	return *this;
+}
+
+str &str::operator=(const char *other)
+{
+	copy_from_raw(other, strlen(other));
+	return *this;
+}
+
+str &str::operator+(const str &next)
+{
+	append(next);
+	return *this;
+}
+
+str &str::operator+(const char *next)
+{
+	append(next);
+	return *this;
+}
+
+str &str::operator+=(const str &next)
+{
+	append(next);
+	return *this;
+}
+
+str &str::operator+=(const char *next)
+{
+	append(next);
+	return *this;
+}
