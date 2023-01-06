fc0c6c15ff4aa36ffcb8c5164adf163ba8b8d171
bellrise <bellrise.dev@gmail.com>
2023-01-06 14:54:57 +0100

    detail: add base_type & same_type traits
    base_type<T>: removes any references on T and returns a raw T
    	T -> T
    	T& -> T
    	T&& -> T
    same_type<T, U>: returns a constexpr bool depending if T == U
    	int, int -> true
    	int, float -> false

--- incl/libcsd/detail.h
+++ incl/libcsd/detail.h
@@ -7,14 +7,45 @@ namespace csd {
 
 /* Utilities for the csd library */
 
-template <typename T>
-concept IsMovable = requires (T a, T v)
-{
-	a = static_cast<T&&>(v);
-};
+/* Similar to std::remove_reference, these trait structs just strip the
+   references off the type. Note that you should be using base_type<T>,
+   and not base_type_s<T>::type. */
 
 template <typename T>
-concept IsComparable = requires (T a, T b)
+struct base_type_s
+{ using type = T; };
+
+template <typename T>
+struct base_type_s<T&>
+{ using type = T; };
+
+template <typename T>
+struct base_type_s<T&&>
+{ using type = T; };
+
+template <typename T>
+using base_type = typename base_type_s<T>::type;
+
+
+template <typename T, typename U>
+struct same_type_s
+{ static constexpr bool result = false; };
+
+template <typename T>
+struct same_type_s<T, T>
+{ static constexpr bool result = true; };
+
+template <typename T, typename U>
+constexpr static bool same_type = same_type_s<T, U>::result;
+
+template <typename T>
+concept IsMovable = requires (base_type<T> a, base_type<T> v)
+{
+	a = static_cast<base_type<T>&&>(v);
+};
+
+template <typename T, typename U>
+concept IsComparable = requires (base_type<T> a, base_type<U> b)
 {
 	a == b;
 };
