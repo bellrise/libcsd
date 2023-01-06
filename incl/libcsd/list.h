766522c5a2bdb5412c9e36393df9fa2a4dd342f4
bellrise <bellrise.dev@gmail.com>
2023-01-06 21:27:17 +0100

    list: fix allocation size algorithm
    The algorithm was causing a segfault, because when the requested
    size was over 512, it would always choose 512 anyway.

--- incl/libcsd/list.h
+++ incl/libcsd/list.h
@@ -6,6 +6,7 @@
 #include <libcsd/detail.h>
 #include <libcsd/str.h>
 #include <stddef.h>
+#include <string.h>
 
 /**
  * @class list<T>
@@ -33,6 +34,7 @@ struct list
 
 	~list()
 	{
+		delete_range(m_ptr, 0, m_space);
 		delete [] m_ptr;
 	}
 
@@ -44,13 +46,15 @@ struct list
 	void append(T copied_value)
 	{
 		resize(++m_len);
-		m_ptr[m_len - 1] = copied_value;
+		m_ptr[m_len - 1] = new T(copied_value);
 	}
 
 	void remove(size_t index)
 	{
 		if (index >= len())
 			throw index_exception(index, 0, len() - 1);
+
+		delete m_ptr[index];
 
 		for (size_t i = index; i < len(); i++)
 			m_ptr[i] = m_ptr[i + 1];
@@ -62,7 +66,7 @@ struct list
 	void remove(V& item)
 	{
 		for (size_t i = 0; i < len(); i++) {
-			if (item == m_ptr[i]) {
+			if (item == *m_ptr[i]) {
 				remove(i);
 				return;
 			}
@@ -75,11 +79,11 @@ struct list
 
 		if (m_len > 1) {
 			for (size_t i = 0; i < len() - 1; i++) {
-				builder += str(m_ptr[i]) + ", ";
+				builder += str(*m_ptr[i]) + ", ";
 			}
-			builder += str(m_ptr[len() - 1]);
+			builder += str(*m_ptr[len() - 1]);
 		} else {
-			builder += str(m_ptr[0]);
+			builder += str(*m_ptr[0]);
 		}
 
 		return builder + ']';
@@ -89,14 +93,14 @@ struct list
 	{
 		if (index >= len())
 			throw index_exception(index, 0, len() - 1);
-		return m_ptr[index];
+		return *(m_ptr[index]);
 	}
 
 	const T& operator[](size_t index) const
 	{
 		if (index >= len())
 			throw index_exception(index, 0, len() - 1);
-		return m_ptr[index];
+		return *(m_ptr[index]);
 	}
 
 private:
@@ -106,52 +110,64 @@ private:
 		if (m_space >= n)
 			return;
 
-		// TODO: there is way too many deletes for any T, especially if there is
-		//       any dynamic memory allocated. list<T> should be changed to store
-		//       an array of pointers, and not an array of elements (along with that,
-		//       this function will be simpler)
-
 		/* Allocate to the nearest power of 2, or 1024 elements if the total
 		   required size is over 1024. This will ensure a small memory footprint
 		   for tiny arrays, but an over-allocation strategy for large arrays. */
 		size_t new_size = 1024;
 		for (int i = 0; i < 10; i++) {
+			if ((size_t) (1 << i) < n)
+				continue;
 			new_size = (1 << i);
-			if (new_size >= n)
-				break;
+			break;
 		}
 
 		if (n > 1024)
-			new_size = ((n << 10) >> 10) + 1024;
+			new_size = ((n >> 10) << 10) + 1024;
 
+		/* Previously empty buffer. */
 		if (m_ptr == nullptr) {
-			m_ptr = new T[new_size];
+			m_ptr = new T*[new_size];
+			zero_range(m_ptr, 0, new_size);
 			m_space = new_size;
 			return;
 		}
 
-		T *old_ptr = m_ptr;
-		m_ptr = new T[new_size];
-		copy_objects(m_ptr, old_ptr, m_space);
+		/* We already allocated something, so we need to create a new buffer
+		   and copy the old pointers to the new buffer. */
+		T **old_ptr = m_ptr;
+
+		m_ptr = new T*[new_size];
+
+		zero_range(m_ptr, 0, new_size);
+		copy_range(m_ptr, old_ptr, m_space);
+
 		delete [] old_ptr;
 		m_space = new_size;
 	}
 
-	template <typename P>
-	void copy_objects(P *new_ptr, P *old_ptr, size_t n)
+	inline void copy_range(T **to_ptr, T **from_ptr, size_t n)
 	{
 		for (size_t i = 0; i < n; i++)
-			new_ptr[i] = old_ptr[i];
+			to_ptr[i] = from_ptr[i];
 	}
 
-	template <csd::IsMovable P>
-	void copy_objects(P *new_ptr, P *old_ptr, size_t n)
+	void delete_range(T **ptr, size_t from, size_t to)
 	{
-		for (size_t i = 0; i < n; i++)
-			new_ptr[i] = static_cast<T&&>(old_ptr[i]);
+		for (size_t i = from; i < to; i++) {
+			if (ptr[i] == nullptr)
+				continue;
+			delete ptr[i];
+			ptr[i] = nullptr;
+		}
+	}
+
+	inline void zero_range(T **ptr, size_t from, size_t to)
+	{
+		for (size_t i = from; i < to; i++)
+			ptr[i] = nullptr;
 	}
 
 	size_t m_space;
 	size_t m_len;
-	T *m_ptr;
+	T **m_ptr;
 };
