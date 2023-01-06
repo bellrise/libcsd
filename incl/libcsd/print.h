934cb11849d232f45396480e123e2849439332e8
bellrise <bellrise.dev@gmail.com>
2023-01-06 21:30:44 +0100

    print: remove the space at the end of each println()

--- incl/libcsd/print.h
+++ incl/libcsd/print.h
@@ -6,38 +6,55 @@
 
 /* Print functions. */
 
-void print(const void *val);
-void print(const char *val);
-void print(const str& val);
-void print(const char val);
-void print(const unsigned char val);
-void print(const short val);
-void print(const unsigned short val);
-void print(const int val);
-void print(const unsigned int val);
-void print(const long val);
-void print(const unsigned long val);
-void print(const float val);
-void print(const double val);
-void print(const bool val);
+void print_single(const void *val);
+void print_single(const char *val);
+void print_single(const str& val);
+void print_single(const char val);
+void print_single(const unsigned char val);
+void print_single(const short val);
+void print_single(const unsigned short val);
+void print_single(const int val);
+void print_single(const unsigned int val);
+void print_single(const long val);
+void print_single(const unsigned long val);
+void print_single(const float val);
+void print_single(const double val);
+void print_single(const bool val);
+
+template <typename T>
+void print(const T* value)
+{
+	print_single(value);
+}
 
 template <StringConvertible T>
 void print(const T& value)
 {
-	print(value.to_str());
+	print_single(value.to_str());
 }
 
 /* Multiple-argument print() functions. */
 
-template <typename... T>
-void print(const T&... values)
+template <typename T, typename... VT>
+void print(const T& value, const VT&... values)
 {
-	((print(values), print(' ')), ...);
+	print_single(value);
+
+	if constexpr (sizeof...(values) != 0) {
+		print_single(' ');
+		print(values...);
+	}
 }
 
-template <typename... T>
-void println(const T&... values)
+template <typename T, typename... VT>
+void println(const T& value, const VT&... values)
 {
-	((print(values), print(' ')), ...);
-	print('\n');
+	print_single(value);
+
+	if constexpr (sizeof...(values) != 0) {
+		print_single(' ');
+		print(values...);
+	}
+
+	print_single('\n');
 }
