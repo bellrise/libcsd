51fb407e4b22902d6147493f0d9d97952270a67b
bellrise <bellrise.dev@gmail.com>
2023-01-08 16:48:10 +0100

    error: add string index to index_exception constructor

--- src/error.cc
+++ src/error.cc
@@ -1,13 +1,20 @@
 /* libcsd/src/error.cc
    Copyright (c) 2022-2023 bellrise */
 
+#include "libcsd/format.h"
 #include <libcsd/error.h>
 
 namespace csd {
 
 index_exception::index_exception(int index)
-	: m_has_bounds(false), m_index(index)
+	: m_has_bounds(false), m_str_index(), m_index(index)
 { }
+
+index_exception::index_exception(const str& str_index)
+	: m_has_bounds(false)
+{
+	m_str_index = csd::format("`{}`", str_index);
+}
 
 index_exception::index_exception(int index, int min, int max)
 	: m_has_bounds(true), m_index(index), m_min(min), m_max(max)
@@ -17,19 +24,19 @@ str index_exception::message() const
 {
 	str err = "index_exception: index ";
 
-	err += str(m_index) + " is out of bounds";
-	if (m_has_bounds) {
-		err.append(" for a range of [").append(m_min).append(", ")
-			.append(m_max).append("]");
-	}
+	err += m_str_index.empty() ? m_index : m_str_index;
+	err += " is out of bounds";
+
+	if (m_has_bounds)
+		err += csd::format(" for a range of [{}, {}]", m_min, m_max);
 
 	return err;
 }
 
 str unpack_exception::message() const
 {
-	str err = "unpack_exception: tried to unpack the value from ";
-	err += "maybe<T>, but no value was present";
+	str err = "unpack_exception: tried to unpack the value from " \
+		"maybe<T>, but no value was present";
 	return err;
 }
 
