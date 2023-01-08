52337d6650d8c0ab2c5261a9ef6aaf643ac9351b
bellrise <bellrise.dev@gmail.com>
2023-01-08 16:02:48 +0100

    format: new csd::format function for "f-strings"
    str format = csd::format("one = {}", 1);
    println(format);  // one = 1

---
+++ incl/libcsd/format.h
@@ -0,0 +1,37 @@
+/* <libcsd/format.h>
+   Copyright (c) 2023 bellrise */
+
+#pragma once
+
+#include <libcsd/str.h>
+
+namespace csd {
+
+inline str format(const str& empty_fmt)
+{
+	return empty_fmt;
+}
+
+/**
+ * @function format
+ * Format the given string with the following arguments.
+ *
+ *  str hello = csd::format("Hello there {}, you are {} yo!", "John", 12);
+ *  println(hello); // Hello there John, you are 12 yo!
+ */
+template <csd::StringConvertible T, typename... VT>
+str format(const str& fmt, T first_arg, VT... args)
+{
+	size_t index;
+	str formatted;
+
+	if ((index = fmt.find("{}")) == str::invalid_index)
+		return fmt;
+
+	formatted += fmt.substr(0, index);
+	formatted += str(first_arg);
+	formatted += format(fmt.substr(index + 2), args...);
+
+	return formatted;
+}
+}
