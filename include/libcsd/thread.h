9cae29a0f8844502d383c7039901496eac7d672a
fenze <contact@fenze.dev>
2023-01-25 21:29:54 +0100

    thread: new type

---
+++ include/libcsd/thread.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <libcsd/map.h>
+
+using pthread_t = unsigned long;
+using fptr_t = void *(*)(void*);
+
+namespace csd
+{
+
+class thread {
+	void *ret;
+	bool _is_running;
+	pthread_t id;
+	void run(fptr_t fptr, void *arg);
+
+	public:
+
+	/* inherited */
+	thread() = default;
+	thread(fptr_t fptr);
+	thread(fptr_t fptr, void *arg);
+	pthread_t getid() const;
+	int join();
+	void *get_result();
+	int detach() const;
+	int cancel() const;
+	void exit();
+	int kill(int sig) const;
+	bool operator==(const thread& other) const;
+	void operator()(fptr_t fptr, void *arg);
+	void operator()(fptr_t fptr);
+};
+
+}
